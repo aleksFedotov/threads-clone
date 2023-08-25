65ac438fc4c3b6277b1425869177c46a735b42e3
aleksFedotov <aleksfedotov.dev@gmail.com>
2023-08-25 06:53:53 +0300

    hot fix redirect after singin + correct user id

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -11,7 +11,7 @@ const Page = async ({ params }: { params: { id: string } }) => {
   const user = await currentUser();
 
   if (!user) return null;
-  const userInfo = await fetchUser(user.id);
+  const userInfo = await fetchUser(params.id);
 
   if (!userInfo?.onboarded) redirect('/onboarding');
 
