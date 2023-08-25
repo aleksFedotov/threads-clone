1da4a0d947bae530e749a055498e9122cb816f60
aleksFedotov <aleksfedotov.dev@gmail.com>
2023-08-25 11:38:01 +0300

    activity page boiler plate

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,6 +1,23 @@
-type Props = {};
+import UserCard from '@/components/cards/UserCard';
+import ProfileHeader from '@/components/shared/ProfileHeader';
+import ThreadsTab from '@/components/shared/TreadsTab';
 
-const Page = (props: Props) => {
+import { profileTabs } from '@/constants';
+import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs';
+import Image from 'next/image';
+import { redirect } from 'next/navigation';
+
+const Page = async () => {
+  const user = await currentUser();
+
+  if (!user) return null;
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect('/onboarding');
+
+  // get notifications
+
   return (
     <div>
       <h1 className="head-text mb-10">Activity</h1>
